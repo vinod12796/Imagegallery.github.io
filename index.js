61101c348199a81f0cc818cef2cf59b46f15cde0
VINOD ANIL PAWAR <84064563+vinod12796@users.noreply.github.com>
2021-10-30 13:22:55 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var fullimgbox =document.getElementById("fullimgbox");
+var fullimg =document.getElementById("fullimg");
+
+function openfullimg(pic){
+    fullimgbox.style.display ="flex";
+    fullimg.src = pic;
+}
+
+function closefullimg(){
+    fullimgbox.style.display ="none";
+
+}
